f20949eb88ed2517f80ed798a81f525f86012fee
Ravi Teli <reenkeshmodi@gmail.com>
2024-09-03 11:47:16 +0530

    change header

--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css';
-import Emp from './Programmer3.svg';
+import Emp from './Freelance-Developer.svg';
 
 
 function Header() {
@@ -8,10 +8,12 @@ function Header() {
     <>
     <div className='Header-container'>
       <div className='latter-container'>
-        <p className='latter l1'>Join</p>
-        <p className='latter l2'>Next</p>
-        <p className='latter l3'>Hire</p><br/>
+        <p className='latter l1'>Refer.</p>
+        <p className='latter l2'>Connect.</p>
+        <p className='latter l3'>Thrive.</p><br/>
+        <p className='latter l4'>"Help your network find their dream job!<br/> Refer friends and contacts to job openings that match their skills."</p>
       </div>
+      <div className="line"></div>
       <div className='logo'>
       <img src={Emp} alt='Logo' height={550} width={550}/ >
       </div>
